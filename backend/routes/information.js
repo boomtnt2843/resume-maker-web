3f925cc3dacde045af14472d974601666a605bdf
Boom.Phattarapong <61021302+boomertnt210943@users.noreply.github.com>
2022-09-06 17:56:40 +0700

    add delete information api

--- backend/routes/information.js
+++ backend/routes/information.js
@@ -4,6 +4,12 @@ const mongoose = require('mongoose');
 
 const authorization = require('../config/authorize');
 const info = require('../models/information');
+const activity = require('../models/activity');
+const education = require('../models/education');
+const experience = require('../models/experience');
+const general = require('../models/generalSkill');
+const language = require('../models/language');
+const technical = require('../models/technicalSkill');
 
 //Frist time you click "Let's Start"
 const createNewInformation = (dataInformation) => {
@@ -118,5 +124,54 @@ router.route('/:ofInfoID')
         })
     })
 
+router.route('/deleteInfo/:id')
+    .delete(authorization, (req,res) => {
+        activity.deleteMany({ofInformation: req.params.id}, (error ,data) => {
+            if(error){
+                //console.log(error);
+                res.status(500).json(error)
+            }
+        })
+        education.deleteMany({ofInformation: req.params.id}, (error ,data) => {
+            if(error){
+                //console.log(error);
+                res.status(500).json(error)
+            }
+        })
+        experience.deleteMany({ofInformation: req.params.id}, (error ,data) => {
+            if(error){
+                //console.log(error);
+                res.status(500).json(error)
+            }
+        })
+        general.deleteMany({ofInformation: req.params.id}, (error ,data) => {
+            if(error){
+                //console.log(error);
+                res.status(500).json(error)
+            }
+        })
+        language.deleteMany({ofInformation: req.params.id}, (error ,data) => {
+            if(error){
+                //console.log(error);
+                res.status(500).json(error)
+            }
+        })
+        technical.deleteMany({ofInformation: req.params.id}, (error ,data) => {
+            if(error){
+                //console.log(error);
+                res.status(500).json(error)
+            }
+        })
+        info.findByIdAndDelete(req.params.id, (error ,data) => {
+            if(error){
+                //console.log(error);
+                res.status(500).json(error)
+            }else{
+                res.status(200).json({
+                    msg: data
+                })
+            }
+        })
+    })
 
 module.exports = router
