e6576dc11452631f5cc8351e1ea7e04e013b16af
Boom.Phattarapong <61021302+boomertnt210943@users.noreply.github.com>
2022-09-10 14:49:23 +0700

    update sign in up

--- frontend-resume/src/components/Home.jsx
+++ frontend-resume/src/components/Home.jsx
@@ -1,6 +1,14 @@
 import '../css/homeCustom.css';
 import HomeNavBar from './HomeNavBar';
 
+const toSignUp = () =>{
+  window.location.href='/signup'
+}
+
+const toSignIn = () =>{
+  window.location.href='#signin'
+}
+
 function Home() {
     return (
       <div>
@@ -8,7 +16,7 @@ function Home() {
         <div className='Main-page' id='home'>
           <h1>SAYNUM</h1>
           <h2>WELCOME TO RESUME BUILDER</h2>
-          <button className="Start-btn" href="#signin">Let's Go</button>
+          <button className="Start-btn" onClick={toSignIn}>Let's Go</button>
         </div>
         <div className="Format-page1-container">
           <div className="Format1-detail">
@@ -30,11 +38,26 @@ function Home() {
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla dolorum ullam est unde impedit labore facilis quia deleniti earum in.</p>
           </div>
         </div>
-        <div className="SignIn-page-container" id='signin'>
-
-        </div>
+          <div className="SignIn-page-container" id='signin'>
+            <h1 className='header-text'>SIGN IN...</h1>
+            <form>
+              <div className="box-signin">
+                <h3>Username</h3>
+                <input type="text" className="signin-input" placeholder='username...' />
+                <h3>Password</h3>
+                <input type="password" className="signin-input" placeholder='password...' />
+              </div>
+              <button className="signin-btn">
+                SIGN IN
+              </button>
+            </form>
+            <button className="signup-page-btn" onClick={toSignUp}>
+              Do You want to get some account?
+            </button>
+          </div>
         <div className="foot-container" />
       </div>
+      
     );
   }
   
