6ab27318d6555cb595d31e5445cc74bc3814dbc4
Boom.Phattarapong <61021302+boomertnt210943@users.noreply.github.com>
2022-10-01 09:36:26 +0700

    test

--- frontend/src/components/Skill.tsx
+++ frontend/src/components/Skill.tsx
@@ -2,7 +2,18 @@ import '../css/navbar.css';
 
 function Skill() {
     return (
-        <div className="skill-container"></div>
+        <div className="skill-container">
+          <div className="general-skill-container">
+            <h1>General Skill</h1>
+            <table className="general-table">
+              <tr>
+                <th>Skill</th>
+                <th>Power</th>
+                <th>Option</th>
+              </tr>
+            </table>
+          </div>
+        </div>
     );
   }
   
