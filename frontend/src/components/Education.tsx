35aa724daf91668d58983a42bb6c9d0f09877c6b
Boom.Phattarapong <61021302+boomertnt210943@users.noreply.github.com>
2022-10-24 22:33:48 +0700

    education UI + edit

--- frontend/src/components/Education.tsx
+++ frontend/src/components/Education.tsx
@@ -12,12 +12,6 @@ function Education() {
 
     const apiUrl = "http://localhost:4200";
 
-    const nameInput = document.getElementById('name') as HTMLInputElement;
-    const degressInput = document.getElementById('degress') as HTMLInputElement;
-    const startInput = document.getElementById('startDate') as HTMLInputElement;
-    const endInput = document.getElementById('endDate') as HTMLInputElement;
-    const educationInput = document.getElementById('education-input') as HTMLDivElement;
-
     const handleInputChange = (
         event: React.ChangeEvent<{ id?: string; value: any }>
       ) => {
@@ -48,6 +42,7 @@ function Education() {
       };
 
       const maxEdu = (num: number) => {
+        const educationInput = document.getElementById('education-input') as HTMLDivElement;
         if (num >= 5){
             educationInput.className="education-input max"
         }else{
@@ -107,6 +102,10 @@ function Education() {
     const submitFormEducation = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         let errorInputCheck = false;
+        const nameInput = document.getElementById('name') as HTMLInputElement;
+        const degressInput = document.getElementById('degress') as HTMLInputElement;
+        const startInput = document.getElementById('startDate') as HTMLInputElement;
+        const endInput = document.getElementById('endDate') as HTMLInputElement;
 
         if(nameInput.value===""){
             textError(nameInput,"plase input name of school/university");
@@ -153,6 +152,43 @@ function Education() {
         parentElement.className = 'box-input';
     }
 
+    const toggleEdit = (elementStr : string) =>{
+        const eduElement = document.getElementById(elementStr) as HTMLDivElement;
+        eduElement.classList.toggle('active');
+    }
+
+    const checkVarEdit = (elementStr : string) =>{
+        const nameEditElement = document.getElementById((elementStr+"-name")) as HTMLTextAreaElement;
+        const degressEditElement = document.getElementById(elementStr+"-degress") as HTMLInputElement;
+        const sDateEditElement = document.getElementById(elementStr+"-start-date") as HTMLInputElement;
+        const eDateEditElement = document.getElementById(elementStr+"-end-date") as HTMLInputElement;
+        let data = {
+            name: nameEditElement.value,
+            degress: degressEditElement.value,
+            startDate: sDateEditElement.value,
+            endDate: eDateEditElement.value
+        };
+        const requestOptionsPost = {
+            method: "PUT",
+            headers: {
+                Authorization: token,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        };
+        fetch(`${apiUrl}/education/edit/${elementStr}`, requestOptionsPost)
+            .then((response) => response.json())
+            .then((res) => {
+                if (res._id) {
+                    toggleEdit(elementStr);
+                    getEducations();
+                } else {
+                    console.log("error");
+                }
+                
+            });
+    }
+
     useEffect(()=>{
         const token = localStorage.getItem("token")
         const myInfoID = localStorage.getItem("Info_id")
@@ -169,55 +205,82 @@ function Education() {
             <div className="education-container">
                 <h1>Education</h1>
                 <div className="education-box">
-                    <table className="education-table">
-                        <thead>
-                            <tr>
-                                <th>location</th>
-                                <th>degress</th>
-                                <th>start date</th>
-                                <th>end date</th>
-                                <th>delete</th>
-                            </tr>
-                        </thead>
-                        <tbody>
+                    <div className="education-show">
                         {educations.map((item: educationInterface,index) =>(
-                            <tr key={index}>
-                                <td>{item.name}</td>
-                                <td>{item.degress}</td>
-                                <td>{String(item.startDate).slice(0,7)}</td>
-                                <td>{String(item.endDate).slice(0,7)}</td>
-                                <td>
-                                    <button onClick={()=>deleteEducation(item._id)}><FaTrashAlt/></button>
-                                </td>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </table>
+                        <div className="edu-item-box" key={index} id={item._id}>
+                            <div className="display-edu">
+                                <div className="location-group">
+                                    <h2>location</h2>
+                                    <p>{item.name}</p>
+                                </div>
+                                <div className="degress-group">
+                                    <h2>degress</h2>
+                                    <p>{item.degress}</p>
+                                </div>
+                                <div className="date-group">
+                                    <div className="start-date">
+                                        <h2>start Date</h2>
+                                        <p>{String(item.startDate).slice(0,7)}</p>
+                                    </div>
+                                    <div className="end-date">
+                                        <h2>end Date</h2>
+                                        <p>{String(item.endDate).slice(0,7)}</p>
+                                    </div> 
+                                </div>
+                                <div className="display-btn-group">
+                                    <button type="button" className="edit-edu-btn" onClick={()=>toggleEdit(item._id)}>Edit</button>
+                                    <button type="button" className="delete-btn" onClick={()=>deleteEducation(item._id)}><FaTrashAlt/></button>
+                                </div>
+                            </div>
+                            <form className="edu-edit-form" id={item._id+"-edit-form"}>
+                                <div className="location-edit-group">
+                                    <h2>location</h2>
+                                    <textarea rows={2} cols={50} className="edit-box" id={item._id+"-name"} defaultValue={item.name} />
+                                </div>
+                                <div className="degress-edit-group">
+                                    <h2>degress</h2>
+                                    <input type="text" className="edit-box" id={item._id+"-degress"} defaultValue={item.degress}/>
+                                </div>
+                                <div className="start-date-edit-group">
+                                    <h2>start Date</h2>
+                                    <input type="month" className="edit-box" id={item._id+"-start-date"} defaultValue={String(item.startDate).slice(0,7)}/>
+                                </div>
+                                <div className="end-date-edit-group">
+                                    <h2>end Date</h2>
+                                    <input type="month" className="edit-box" id={item._id+"-end-date"} defaultValue={String(item.endDate).slice(0,7)}/>
+                                </div>
+                                <div className="edit-btn-group">
+                                    <button type="button" className="update-btn" onClick={()=>checkVarEdit(item._id)}>Update</button>
+                                    <button type="button" className="cancel-btn" onClick={()=>toggleEdit(item._id)}>Cancel</button>
+                                </div>
+                            </form>
+                        </div>))}
+                    </div>
                     <div className="education-input" id="education-input">
                         <h1>IT'S MAXIMUM EDUCATION!</h1>
                         <form className="education-form" id="education-form" onSubmit={submitFormEducation}>
                             <h2>Add Education</h2>
                             <div className="box-input">
-                                <p>location</p>
-                                <input type="text" className="location-input" id="name" onChange={handleInputChange} />
+                                <p>School / University</p>
+                                <input type="text" className="info-input" id="name" onChange={handleInputChange} placeholder="school or university..." />
                                 <small>something error</small>
                             </div>
                             <div className="box-input">
                                 <p>degress</p>
-                                <input type="text" className="degress-input" id="degress" onChange={handleInputChange} />
+                                <input type="text" className="info-input" id="degress" onChange={handleInputChange} placeholder="degress..." />
                                 <small>something error</small>
                             </div>
                             <div className="box-input">
                                 <p>start date</p>
-                                <input type="date" className="date-input" id="startDate" onChange={handleInputChange} />
+                                <input type="month" className="info-input" id="startDate" onChange={handleInputChange} />
                                 <small>something error</small>
                             </div>
                             <div className="box-input">
                                 <p>end date</p>
-                                <input type="date" className="date-input" id="endDate" onChange={handleInputChange} />
+                                <input type="month" className="info-input" id="endDate" onChange={handleInputChange} />
                                 <small>something error</small>
                             </div>
-                            <button type="submit">add</button>
+                            <button type="submit" className="edu-submit-btn">Add</button>
                         </form>
                     </div>
                 </div>
